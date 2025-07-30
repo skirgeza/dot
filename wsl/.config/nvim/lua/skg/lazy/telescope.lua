f2380ac271a7a9fab4ae5bcdb26c80453f5b1fa5
skg <3499023+skirgeza@users.noreply.github.com>
2025-07-30 17:19:15 +0200

    nvim add follow and hidden to telescope find_files

--- wsl/.config/nvim/lua/skg/lazy/telescope.lua
+++ wsl/.config/nvim/lua/skg/lazy/telescope.lua
@@ -6,7 +6,12 @@ return {
 	config = function()
 		require('telescope').setup({})
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>pf', function()
+            builtin.find_files({
+                follow = true,
+                hidden = true,
+            })
+        end)
 		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>ps', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
